cb7b3da0f0c4f5c576c57bca7a1ba110943d26bf
unknown <intern2021@ubestream.com>
2021-12-18 03:20:37 +0800

    87

---
+++ DOE/final/test1.R
@@ -0,0 +1,53 @@
+setwd("C:/Users/user/Desktop/R/DOE/Data")
+library(readr)
+library(dplyr)
+library(stringr)
+df =  readxl::read_xlsx("test.xlsx")
+
+# type = c("毒品", "機車竊盜", "汽車竊盜", "住宅竊盜", "強制性交", "強盜", "搶奪")
+
+df = df %>% filter((type=="毒品")|(type=="機車竊盜")|(type=="汽車竊盜")|(type=="住宅竊盜")|(type=="強制性交")|(type=="強盜")|(type=="搶奪"))
+
+d_trans = function(raw_d){
+  
+  y = str_sub(raw_d, start = 1, end = 3) %>% as.numeric()
+  m = str_sub(raw_d, start = 4, end = 5)
+  d = str_sub(raw_d, start = 6, end = 7)
+  ymd = paste(as.character(y+1911), m, d, sep = "-")
+  return(ymd)
+  
+}
+
+loca_trans = function(raw_loca){
+  
+  return(str_sub(raw_loca, start = 1, end = 3))
+  
+}
+df$district = df$loca %>% loca_trans() 
+tmp = df$dt %>% d_trans()  # a=as.POSIXct("2014-05-01", format="%Y-%m-%d")
+date = c()
+for(i in tmp){
+  qq = as.POSIXct(i, format="%Y-%m-%d")
+  date = append(date, qq)
+}
+
+
+df = df %>% na.omit()
+
+df = df[,-c(2,3)]
+
+
+df %>% head()
+
+# df = df %>% filter()
+
+df$district %>% as.factor() %>% summary()
+
+df = df %>% filter((district!="外國")&(district!="台灣海")&(district!="金門縣")&(district!="連江縣")&(district!="澎湖縣")) 
+# df$district %>% as.factor() %>% summary()
+df %>% head()
+
+#type data 
+
+
+
---
+++ DOE/final/test2.R
@@ -0,0 +1,34 @@
+setwd("C:/Users/user/Desktop/R/DOE/Data")
+library(dplyr)
+library(stringr)
+df = readxl::read_xlsx("crim106.xlsx")
+
+df = df %>% filter((type=="毒品")|(type=="機車竊盜")|(type=="汽車竊盜")|(type=="住宅竊盜")|(type=="強制性交")|(type=="強盜")|(type=="搶奪"))
+
+d_trans = function(raw_d){
+  
+  y = str_sub(raw_d, start = 1, end = 3) %>% as.numeric()
+  m = str_sub(raw_d, start = 4, end = 5)
+  d = str_sub(raw_d, start = 6, end = 7)
+  ymd = paste(as.character(y+1911), m, d, sep = "-")
+  return(ymd)
+  
+}
+
+loca_trans = function(raw_loca){
+  
+  return(str_sub(raw_loca, start = 1, end = 3))
+  
+}
+df$district = df$loca %>% loca_trans() 
+tmp = df$dt %>% d_trans()  # a=as.POSIXct("2014-05-01", format="%Y-%m-%d")
+date = c()
+for(i in tmp){
+  qq = as.POSIXct(i, format="%Y-%m-%d")
+  date = append(date, qq)
+}
+
+df = df[,-c(2,3)]
+df$date = date
+
+
